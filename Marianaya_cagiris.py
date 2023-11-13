43dc8da2fe12349918e9ccdd7871f8652edc6e0a
MarianaDarkSoul <150658521+MarianaDarkSoul@users.noreply.github.com>
2023-11-13 13:46:31 +0500

    Marianaya_cagiris.py
    Ben seni marianaya cagiriyorum

---
+++ Marianaya_cagiris.py
@@ -0,0 +1,7 @@
+import os,sys,time
+def kata(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1./12)
+kata('''\033[91mBy:Dark_Soul:\033[00mSelam nasilsin \n bende iyiyim \n benim adim Dark Soul \n ben Mariana hack teamin bir uyesiyim \n eger sende bizim hack teamimiza katilmayi istiyorsan \n Telegram: @byhyperinios \n Seni bekler''')
